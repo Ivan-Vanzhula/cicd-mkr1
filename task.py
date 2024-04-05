c02c8f46297a20ad923bd1f59bb2a2a2a75064e9
Ivan-Vanzhula <ivan.vanzula@knu.ua>
2024-04-05 15:02:12 +0300

    Create a parse and counting functions

---
+++ task.py
@@ -0,0 +1,27 @@
+import re
+
+
+def load_text(path="input.txt"):
+    """Return text from the file"""
+    file = open(path)
+    return file.read()
+
+
+def parse_words(text):
+    """Split text to words"""
+    words = re.split(r'[.,?!:;\s]+', text)
+    print(words)
+    return len(words)
+
+
+def parse_strings(text):
+    """Split text to strings"""
+    strings = re.split(r'[.?!]+', text)
+    print(strings)
+    return len(strings)
+
+
+if __name__ == "__main__":
+    text = load_text()
+    print(parse_words(text))
+    print(parse_strings(text))
